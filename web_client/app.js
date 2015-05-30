8be4a9370f5b89744b7f11eb823376d37fdd026d
Georgy Borisenko <gosha-bor@yandex.ru>
2015-05-30 16:26:04 +0300

    Showing posts (doesnt work)

--- web_client/app.js
+++ web_client/app.js
@@ -1,11 +1,20 @@
-var RestSystemServer = "localhost:12888"
+var RestSystemServer = "http://localhost:12888"
 
 $(document).ready(function(){
 	$.ajax({
 		url: RestSystemServer,
 		method: "POST",
-		data: {action: "get_posts"}
+		data: JSON.stringify({action: "get_posts"}),
 	}).done(function(res) {
-		  console.log(res)
+		console.log(res)
+		data = JSON.parse(res)
+		for(var p in data.posts)
+		{
+			e = $("<div class='post'><div class='content'></div><div class='like-panel'><div class='likes'></div><div class='dislikes'></div></div></div>")
+			$(".posts").append(e)
+			e.find(".post .content").text(p.content)
+			e.find(".post .like-panel .likes").text(p.like)
+			e.find(".post .like-panel .dislikes").text(p.dislike)
+		}
 	});
 });
